b4f823e73a40f33adfc16f3ffefa1c0824aa90d3
Alex Greenall <alex@ckx-ily.cn>
2021-10-28 09:01:01 +0000

    ~

--- chess.c
+++ chess.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+const char *whitePieces = "RNBQKP";
+const char *blackPieces = "rnbqkp";
+const char *horizontal = "ABCDEFGH";
+
 _Bool enableColor = 0;
 
 int playSide = 1;
@@ -14,6 +18,19 @@ char buffer[80];
 
 int i = 0, j = 0;
 char chessBoard2[8][8];
+int srcX, srcY, dstX, dstY;
+int srcY1, dstY1;
+int dx, dy;
+char srcPiece, dstPiece;
+
+char *potentialKingRookSwitch;
+
+int tx, ty;
+int check_return_position;
+_Bool hasAttack;
+int atkX, atkY;
+
+#define RET_CHECK_RETURN_POSITION_O3_SWITCH_1 0
 
 int main(int argc, char *argv[]) {
   if (argc > 2) {
@@ -38,7 +55,10 @@ init_cless:
   memset(oooPossible, 1, 2);
 
 print_chess_board:
-  printf("  ABCDEFGH\n +--------+\n");
+  printf("\033[H\033[J");
+  fflush(stdout);
+
+  printf("  %s\n +--------+\n", horizontal);
   for (i = 0; i < 8; i++) {
     printf("%d|", 8 - i);
     for (j = 0; j < 8; j++) {
@@ -67,7 +87,7 @@ print_chess_board:
     printf("|%d\n", 8 - i);
   }
 
-  printf(" +--------+\n  ABCDEFGH\n\n");
+  printf(" +--------+\n  %s\n\n", horizontal);
 
 user_input:
   printf("%s> ", playSide ? "white" : "black");
@@ -79,13 +99,98 @@ parse_user_input:
   if (strlen(buffer) != 6) {
     goto invalid_user_input;
   }
+  
+  if (buffer[2] != ',' && buffer[2] != '-') {
+    goto invalid_user_input;
+  }
+  
+  srcX = toupper(buffer[0]) - 'A';
+  srcY = buffer[1] - '0';
+  dstX = toupper(buffer[3]) - 'A';
+  dstY = buffer[4] - '0';
+  
+  if (srcX < 0 || srcX >= 8 || dstX < 0 || dstX >= 8
+      || srcY < 0 || srcY >= 8 || dstY < 0 || dstY >= 8) {
+    goto invalid_user_input;
+  }
 
-  return 0;
+  srcY1 = 7 - srcY;
+  dstY1 = 7 - dstY;
+
+verify_user_input:  
+  memcpy(chessBoard2, chessBoard, 64);
+  srcPiece = chessBoard2[srcY1][srcX];
+  dstPiece = chessBoard2[dstY1][dstX];
+  
+  if ((playSide && !strchr(whitePieces, srcPiece))
+      || (!playSide && !strchr(blackPieces, srcPiece))) {
+    printf("cannot move from %c%d, because there's not your piece.",
+           horizontal[srcX], srcY);
+    goto pause_reprint;
+  }
+
+  potentialKingRookSwitch = NULL;
+  if (playSide == 1 && srcPiece == 'K' && dstPiece == 'R'
+      && srcY == 7 && dstY == 7) {
+    potentialKingRookSwitch = chessBoard[srcY1];
+  } else if (playSide == 0 && srcPiece == 'k' && dstPiece == 'r'
+             && srcY == 0 && dstY == 0) {
+    poteltialKingRookSwitch = chessBoard[srcY1];
+  }
+
+    if (srcY == 7 && dstY == 7 && srcX == 4 && dstX == 0) {
+      if (!oooPossible[playSide]) {
+        printf("cannot perform king-rook switch: king or rook already moved");
+        goto pause_reprint;
+      }
+
+      for (i = 1; i < 4; i++) {
+        if (chessBoard2[7][i] != '.') {
+          printf("cannot perform king-rook switch: blocked by %c%d",
+                 horizontal[i], srcY);
+          goto pause_reprint;
+        }
+
+        ty = srcY1;
+        tx = i;
+        check_return_position = RET_CHECK_RETURN_POSITION_O3_SWITCH_1;
+        hasAttack = 0;
+        goto check_position_attacked;
+ret_check_position_attacked_o3_switch1:
+        if (hasAttack) {
+          printf("cannot perform king-rook switch: position %c%d attacked by %c%d",
+                 horizontal[srcX], srcY, horizontal[atkX], 7 - atkY);
+          goto pause_reprint;
+        }
+      }
+      goto check_if_mate;
+    }
+  }
+
+  goto program_end;
+
+check_if_mate:
+  goto finish_move_piece;
+
+finish_move_piece:
+  memcpy(chessBoard, chessBoard2, 64);
+  goto print_chess_board;
 
 invalid_user_input:
   printf("  - invalid input.\n");
   printf("    expected: [source],[dest]\n");
-  printf("    example:  e2,e4\n");
-  return 0;  
-}
+  printf("          or: [source]-[dest]\n");
+  printf("     example: e2,e4\n");
+  printf("          or: e2-e4\n");
 
+pause_reprint:
+  getchar();
+  goto print_chess_board;
+
+program_end:
+  printf("program ended.\n");
+  return 0;
+
+check_position_attacked:
+  goto ret_check_position_attacked_o3_switch1;
+}
